d025a9d4dc746ed2c2978ca87dadb7f9a9abd335
Hanno Rein <hanno@hanno-rein.de>
2012-10-04 18:03:05 -0400

    Added simple post receiving function

--- feastshare/feastshare/classes/FSMessageViewerViewController.m
+++ feastshare/feastshare/classes/FSMessageViewerViewController.m
@@ -5,7 +5,7 @@
 //  Created by Craig Howarth on 10/4/12.
 //  Copyright (c) 2012 Feast. All rights reserved.
 //
-
+#import "Parse/Parse.h"
 #import "FSMessageViewerViewController.h"
 
 @interface FSMessageViewerViewController ()
@@ -21,6 +21,20 @@
         // Custom initialization
     }
     return self;
+}
+
+-(void)getListOfPosts{
+    PFQuery *query = [PFQuery queryWithClassName:@"Post"];
+    [query whereKey:@"receiverHash" equalTo:[PFUser currentUser]];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error) {
+            // The find succeeded.
+            NSLog(@"Successfully retrieved %d posts.", objects.count);
+        } else {
+            // Log details of the failure
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }
+    }];
 }
 
 - (void)viewDidLoad
